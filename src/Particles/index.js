7b4559ed7edd7a31d954592676ed3611b0686d1d
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-08-19 14:02:59 +0900

    Fix

--- src/Particles/index.js
+++ src/Particles/index.js
@@ -12,14 +12,19 @@ class Particles extends THREE.Group {
     // geometory
     this._geometry = new THREE.BufferGeometry();
     const vertexPositions = [];
+    const seedValues = [];
     for (let index = 0; index < this._particleNum; index++) {
       vertexPositions.push(random(-256, 256)); // x
       vertexPositions.push(random(-256, 256)); // y
       vertexPositions.push(random(-256, 256)); // z
+      seedValues.push(random(-100, 100));
     }
 
     const vertices = new Float32Array(vertexPositions);
-    this._geometry.addAttribute('position', new THREE.BufferAttribute(vertices,3));
+    const seeds = new Float32Array(seedValues);
+
+    this._geometry.addAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    this._geometry.addAttribute('seed', new THREE.BufferAttribute(seeds, 1));
 
     // material
     this._material = new THREE.ShaderMaterial({
@@ -45,8 +50,9 @@ class Particles extends THREE.Group {
 
   update() {
     this._material.uniforms.time.value += 1;
-    this.rotation.x += 0.0005;
-    this.rotation.z += 0.0005;
+    this.rotation.x -= 0.0005;
+    this.rotation.y -= 0.0005;
+    this.rotation.z -= 0.0005;
   }
 }
 
--- src/shader/vertex.glsl
+++ src/shader/vertex.glsl
@@ -1,4 +1,5 @@
 attribute vec3 color;
+attribute float seed;
 
 uniform float time;
 
@@ -6,7 +7,11 @@ varying vec4 vMvPosition;
 varying vec3 vColor;
 
 void main() {
-  vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+  float positionX = position.x + cos(time / seed * 1.0) * seed / 20.0;
+  float positionY = position.y + sin(time / seed * 2.0) * seed / 20.0;
+  float positionZ = position.z + cos(time / seed * 2.0) * seed / 20.0;
+  vec4 newPos = vec4(positionX, positionY, positionZ, 1.0);
+  vec4 mvPosition = modelViewMatrix * newPos;
   vMvPosition = mvPosition;
   vColor = vec3(0.5, 0.5, 0.5);
 
